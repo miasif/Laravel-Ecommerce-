38b6545e34f6aca09242339d745f7d82dc510b8f
miasif <minhazul.islam.asif@gmail.com>
2021-08-23 17:23:07 +0600

    icon

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -12,21 +12,21 @@ class WishlistController extends Controller
     public function adWishlist($id)
     {
         if(!Auth::Check()){
-            return  response()->json(['error' => 'At first Loging your account']);      
+            return  response()->json(['error' => 'At first Loging your account']);
         }
-    
+
         $userid = Auth::user()->id;
         $check = DB::table('wishlists')->where('user_id', $userid)->where('product_id', $id)->first();
-    
+
         $data = array(
         'user_id' => $userid,
         'product_id' => $id);
-    
+
         if ($check) {
             DB::table('wishlists')->where('user_id', $userid)->where('product_id', $id)->delete();
             // User::where('id',$id)->delete();
-            
-            return response()->json(['error' => 'Product remove from wishlist']);	 
+
+            return response()->json(['error' => 'Product remove from wishlist']);
         }else{
             DB::table('wishlists')->insert($data);
             return response()->json(['success' => 'Product Added on wishlist']);
